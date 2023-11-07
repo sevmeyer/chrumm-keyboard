1001f048e16a3540ad3f1d1a09343afa3df1e6ca
Severin Meyer <hello@sev.dev>
2023-11-07 14:25:42 +0100

    Revise matrix comments in firmware

--- firmware/chrumm/matrix.c
+++ firmware/chrumm/matrix.c
@@ -17,6 +17,11 @@ static void report(uint key, bool signal);
 
 void matrix_init()
 {
+    //                         Pullup
+    //              Switch  .--[ R ]-- V+
+    //       Diode  __|__   |
+    // Row ---|<|---O   O---+--------- Column
+
     for (uint r = 0; r < MATRIX_ROWS; ++r) {
         const uint pin = rowPins[r];
         gpio_init(pin);
@@ -35,10 +40,10 @@ void matrix_init()
 
 void matrix_tick()
 {
-    // The columns are set to GPIO_IN with pull-up and therefore
-    // read 1 by default. All rows are set to 1. During the scan,
-    // one row at a time is set to 0. If a switch is pressed,
-    // the connected column is grounded and reads 0 as well.
+    // The columns are pulled up and therefore read 1 by default.
+    // All rows are set to 1. During the scan, one row at a time
+    // is set to 0. If a switch is pressed, the connected column
+    // is grounded and reads 0 as well.
 
     for (uint r = 0; r < MATRIX_ROWS; ++r) {
         gpio_put(rowPins[r], 0);
@@ -57,31 +62,42 @@ void matrix_tick()
 
 static void debounce(uint key, bool signal)
 {
-    // The goal is to detect state changes in a noisy signal.
-    // One byte is stored per switch. Its Least Significant bit
-    // represents the next switch state. The remaining bits
-    // represent a saturation counter. When the input is equal to
-    // the LSb, the counter is incremented, otherwise decremented.
-    // Once the saturation threshold is reached, we report a
-    // state change, reset the counter, and invert the LSb.
-    //
-    // 1100000 00000000011111 111  Ideal signal
-    // 1101000 00100000011011 111  Noisy signal
-    // 0010123 00100000012123 000  Saturation counter (threshold = 3)
-    // 0000000 11111111111111 000  Next switch state (!current state)
-    // 0020246 11311111135357 000  State variable (saturation*2 + next)
-    // ......0 .............1 ...  State change report
-    //
-    // Partially based on debounce.c by Kenneth A. Kuhn
+    // EvenOdd debounce algorithm
+    // Based on the integrator algorithm by Kenneth A. Kuhn
     // https://www.kennethkuhn.com/electronics/debounce.c
+    //
+    // The progress is maintained in one byte per switch.
+    // The lowest bit represents the inverse of the current
+    // switch state. It is inverted so that the starting
+    // value is zero and can be auto-initalized in an array.
+    // The remaining bits are used as a hysterisis counter.
+    //
+    // .-------------.-.
+    // |7 6 5 4 3 2 1|0|
+    // '-------------'-'
+    // Hysterisis    Inverted
+    // counter       switch state
 
     static uint8_t states[MATRIX_ROWS*MATRIX_COLS] = {0};
     uint8_t state = states[key];
+
+    // The hysterisis counter reflects how often the signal
+    // has matched the state bit in recent history. We add
+    // or subtract a value of 2 to jump over the state bit.
+    //             _
+    //           _/ \_   _ Counter
+    //     _   _/     \_/
+    // ___/ \_/
+    // ! ! = ! = = = ! ! = Signal
 
     if (signal == state % 2)
         state += 2;
     else if (state >= 2)
         state -= 2;
+
+    // When the signal has fully saturated the counter,
+    // we assume that the switch has flipped its state.
+    // Thus we invert the state bit and reset the counter.
 
     if (state >= MATRIX_DEBOUNCE_TICKS * 2) {
         report(key, signal);
